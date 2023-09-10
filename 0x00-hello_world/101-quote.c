3440075ccb25c65938774a42a523543d7aba5860
KojoAnokye <peek1482@gmail.com>
2023-09-10 00:06:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main - A C program that prints a line of the standard error
- * Return 1 (success)
+ * Return: 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is userful\" - Dora korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is userful\" - Dora kor
+par, 2015-10-19\n", stdout);
 	return (1);
 }
